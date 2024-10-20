6dcede2d63fd36c46507cb547fd5b6ffeb7f8cb5
ashraf9932 <ashraf.neizk@gmail.com>
2024-10-20 17:01:17 +0300

    Footer added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,11 @@ const App = () => {
         <Route path="/" element={<ProductList />} />
         <Route path="/add-product" element={<AddProduct />} />
       </Routes>
+      <footer>
+        <p>Scandiweb Test assignment <br />
+          © Copyright ©2024 All rights reserved | This website is made by <a href='https://www.linkedin.com/in/ashraf-sayed22/' target='_blanc'>Ashraf Sayed</a>
+        </p>
+      </footer>
     </Router>
   );
 };
